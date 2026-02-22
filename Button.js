dc00dffad83a65fc36c8193a02dcf57fc8e2aefb
Naveen S S <naveensupa13@gmail.com>
2026-02-22 16:44:31 +0530

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,24 @@
+function changeText(){
+    document.getElementById("b1").innerHTML="<p>Applied</p>";
+    
+}
+function chage(){
+    document.getElementById("b2").innerHTML="<p>Applied</p>";
+    
+}
+function change(){
+    document.getElementById("b3").innerHTML="<p>Applied</p>";
+    
+}
+function change1(){
+    document.getElementById("b4").innerHTML="<p>Applied</p>";
+    
+}
+function change2(){
+    document.getElementById("b5").innerHTML="<p>Applied</p>";
+    
+}
+function change3(){
+    document.getElementById("b6").innerHTML="<p>Applied</p>";
+    
+}
